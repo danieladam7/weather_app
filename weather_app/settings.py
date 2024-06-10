6ac30a753c3d90fc8ffc081398e74c449bb4138f
Daniel Adam <danielyosef@gmail.com>
2024-06-11 01:13:25 +0300

    updating settings for deployment to aws

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -52,8 +52,12 @@ WSGI_APPLICATION = 'weather_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USERNAME'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('RDB_HOSTNAME'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
